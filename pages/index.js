8a39cd6861c5ec7eb4a6adc4b854d6aafa6b8469
ztaz311 <ztaz31197@gmail.com>
2020-09-11 17:57:57 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import Head from 'next/head'
 import { Container } from '../components/main'
 import { Header } from '../components/Layout/header'
+import Menu from '../components/Aside/Menu'
 export default function Home() {
-  return (<>
-    <Header />
-    <Container>
-    </Container>
-  </>
+  return (
+    <>
+      <Header />
+      <Container>
+        <Menu />
+      </Container>
+    </>
   )
 }
