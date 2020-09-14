a397f230a5872488ac7d457f6a6ada11449d9f01
ngocgiangit124 <ngocgiangit124@gmail.com>
2020-09-14 09:40:04 +0700

    them icon lamf header

--- components/Layout/header.js
+++ components/Layout/header.js
@@ -12,14 +12,21 @@ export function Header() {
                     </div>
                     <div >
                         <div className={`relative ${HeaderCss.hspec50} ${HeaderCss.wspec450}`}>
-                            <input className={`relative pr-8 pl-8 rounded-full py-2 px-4 ${HeaderCss.hspec50} ${HeaderCss.wspec450}`} value="giang"></input>
-                            <button className={`absolute h-10 w-10`}><img src="../img/logo.png" /></button>
+                            <input placeholder="Tìm kiếm" className={`bg-colorinput relative pr-8 pl-8 rounded-full py-2 px-4 h-12  ${HeaderCss.wspec450}`} ></input>
+                            <button className={`mr-1 absolute right-0 p-3 h-12 w-12`}><img src="../img/icon-search.png" /></button>
                         </div>
                     </div>
-                    <div >
-                        <div>
-                            <input className="w-30" value="giang"></input>
+                    <div className={`h-12 w-64 flex items-center justify-between`}>
+                        <div className="flex items-center">
+                            <div className="mr-4" ><img className="rounded" src="../img/avatar.png" alt="logo" /></div>
+                            <div>Hoàng Văn Thái</div>
+                            <div className="ml-2">
+                                <svg className="w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
                         </div>
+                        <div><img src="../img/chuong.png" alt="logo" /></div>
                     </div>
                 </div >
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,25 @@ module.exports = {
         // Use *.tsx if using TypeScript
         './pages/**/*.js',
         './components/**/*.js'
-    ]
-    // ...
+    ],
+    theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+        },
+        extend: {
+            backgroundColor: {
+                colorinput: '#F3F4F7',
+            },
+            colors: {
+                cyan: '#9cdbff',
+            },
+            spacing: {
+                '96': '24rem',
+                '128': '32rem',
+            }
+        }
+    }
 }
