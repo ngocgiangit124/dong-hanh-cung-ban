9addac621b95a203ee31796adc0b83f8b4723611
ngocgiangit124 <ngocgiangit124@gmail.com>
2020-09-18 15:41:11 +0700

    fix

--- components/Layout/header.js
+++ components/Layout/header.js
@@ -56,7 +56,6 @@ export const Header = memo(() => {
                                         <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
                                     </svg>
                                 </div>
-                                {/* {state.button2 && <DropLogin />} */}+
                                 {state.button2 && <DropMenu onClose={() => onOpenInfo()} />}
                             </div>
 
